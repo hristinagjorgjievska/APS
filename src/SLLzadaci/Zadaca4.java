7eca6ec9861e40ab1718b90af94a94c54b46b4e2
Hristina Gjorgjievska <hristinna791@gmail.com>
2025-06-02 18:38:50 +0200

    Initial commit

--- src/SLLzadaci/Zadaca4.java
+++ src/SLLzadaci/Zadaca4.java
@@ -1,4 +1,27 @@
 package SLLzadaci;
 
+import java.util.Scanner;
+
 public class Zadaca4 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        SLL<String> list = new SLL<String>();
+        for (int i = 0; i < n; i++) {
+            list.insertLast(cin.next());
+        }
+        int L = cin.nextInt();
+        System.out.println(list);
+
+        SLLNode<String> tmp = list.getFirst();
+
+       while (tmp != null){
+           if (tmp.element.length() > L){
+               list.insertBefore("Outlier", tmp);
+           }
+           tmp = tmp.succ;
+       }
+
+        System.out.println(list);
+    }
 }
